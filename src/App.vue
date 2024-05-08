6667e18d32682be66e08042521d3df4c9ccb7533
Kave Bahraman <sadegh.b218@gmail.com>
2024-05-08 19:13:17 +0330

    static-site with vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header />
+    <router-view></router-view>
+    <Footer />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    Footer,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Global styles for the entire app */
+body {
+  font-family: Consolas, monospace;
+  margin: 0;
+  padding: 0;
+  background-color: #1e1e1e;
+  /* Dark background color */
+  color: #d4d4d4;
+  /* Light text color */
 }
+
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+/* Header styles */
+header {
+  background-color: #252526;
+  /* VS Code header background color */
+  padding: 10px 20px;
+}
+
+nav ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+nav ul li {
+  display: inline;
+  margin-right: 10px;
+}
+
+nav ul li a {
+  color: #d4d4d4;
+  text-decoration: none;
+}
+
+nav ul li a:hover {
+  text-decoration: underline;
+}
+
+/* Footer styles */
+footer {
+  background-color: #252526;
+  /* VS Code footer background color */
+  padding: 20px;
+  color: #d4d4d4;
+  text-align: center;
+}
+
+/* Add more global styles as needed */
 </style>
